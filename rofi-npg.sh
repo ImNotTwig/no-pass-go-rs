a21f717c4010e5c2853b082e483a93644260af96
TwigTheCat <TwigTheCat@disroot.org>
2023-12-29 17:13:40 -0600

    changes ydotool to dotool in rofi script

--- rofi-npg.sh
+++ rofi-npg.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-/home/ly/.local/bin/npg list
+/home/ly/Documents/programming/rust/npg/target/release/npg list >> /dev/null
 
-ACCOUNT_PATH="$(/home/ly/.local/bin/npg list | fuzzel --dmenu )"
+ACCOUNT_PATH="$(/home/ly/Documents/programming/rust/npg/target/release/npg list | fuzzel --dmenu )"
 if [ -z "$ACCOUNT_PATH" ]; then
 	return
 fi
@@ -13,18 +13,19 @@ if [ -z "$OPTION" ]; then
 fi
 
 if [ "$OPTION" = "autotype" ]; then
-	PASSWORD="$(/home/ly/.local/bin/npg show -a "$ACCOUNT_PATH" | head -n1)"
-	USERNAME="$(/home/ly/.local/bin/npg show -a "$ACCOUNT_PATH" | grep "username:" | awk '{print $2}')"
-	doas ydotool type "$USERNAME"
-	doas ydotool key 15:1 15:0
-	doas ydotool type "$PASSWORD"
+	PASSWORD="$(/home/ly/Documents/programming/rust/npg/target/release/npg show -a "$ACCOUNT_PATH" | head -n1)"
+	USERNAME="$(/home/ly/Documents/programming/rust/npg/target/release/npg show -a "$ACCOUNT_PATH" | grep "username:" | awk '{print $2}')"
+	echo type "$USERNAME" | doas dotool
+	echo "$USERNAME"
+	echo key tab | doas dotool
+	echo type "$PASSWORD" | doas dotool
 elif [ "$OPTION" = "password" ]; then
-	PASSWORD="$(/home/ly/.local/bin/npg show -a "$ACCOUNT_PATH" | head -n1)"
-	doas ydotool type "$PASSWORD"
+	PASSWORD="$(/home/ly/Documents/programming/rust/npg/target/release/npg show -a "$ACCOUNT_PATH" | head -n1)"
+	echo type "$PASSWORD" | doas dotool
 elif [ "$OPTION" = "username" ]; then
-	USERNAME="$(/home/ly/.local/bin/npg show -a "$ACCOUNT_PATH" | grep "username:" | awk '{print $2}')"
-	doas ydotool type "$USERNAME"
+	USERNAME="$(/home/ly/Documents/programming/rust/npg/target/release/npg show -a "$ACCOUNT_PATH" | grep "username:" | awk '{print $2}')"
+	echo type "$USERNAME" | doas dotool
 elif [ "$OPTION" = "email" ]; then
-	EMAIL="$(/home/ly/.local/bin/npg show -a "$ACCOUNT_PATH" | grep "username:" | awk '{print $2}')"
-	doas ydotool type "$EMAIL"
+	EMAIL="$(/home/ly/Documents/programming/rust/npg/target/release/npg show -a "$ACCOUNT_PATH" | grep "username:" | awk '{print $2}')"
+	echo type "$EMAIL" | doas dotool
 fi
